5ac00ccc4ef1852b6ed9304de31463876baacd7c
AlejoErwin <alejo.gutierrez.erwin@gmail.com>
2021-11-02 18:58:17 -0400

    se configuro el ms gateway

--- src/main/java/arquitectura/software/msgateway/MsGatewayApplication.java
+++ src/main/java/arquitectura/software/msgateway/MsGatewayApplication.java
@@ -2,8 +2,10 @@ package arquitectura.software.msgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsGatewayApplication {
 
 	public static void main(String[] args) {
