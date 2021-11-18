915bb9fed90cc49beb25aa7108d259f3b8005cf0
AlejoErwin <alejo.gutierrez.erwin@gmail.com>
2021-11-18 16:13:51 -0400

    se configuro el cors para conectar con el frontend

--- src/main/java/arquitectura/software/msgateway/MsGatewayApplication.java
+++ src/main/java/arquitectura/software/msgateway/MsGatewayApplication.java
@@ -3,9 +3,11 @@ package arquitectura.software.msgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableEurekaClient
+@CrossOrigin(origins = "*", allowedHeaders = "*" )
 public class MsGatewayApplication {
 
 	public static void main(String[] args) {
